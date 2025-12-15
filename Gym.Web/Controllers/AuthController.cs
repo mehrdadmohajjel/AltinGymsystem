7b3c529b99205503e3b9c0f8f8da45f327b22a82
Mehrdad <Mehrdad@DESKTOP-PP3DG4J>
2025-12-16 00:06:18 +0330

    Web implemented

---
+++ Gym.Web/Controllers/AuthController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Gym.Web.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
